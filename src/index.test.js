1668295f32bae87c487038b9d8d7309cd3af9a8f
stefan <stefanburt@gmail.com>
2021-03-18 23:04:16 +0000

    clean up

---
+++ src/index.test.js
@@ -0,0 +1,5 @@
+describe("indedx page", () => {
+  test("Add two numbers", () => {
+    expect(2).toEqual(2);
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,7 @@
-const jsonArray = {
-  results: [
-    {
-      assetId: "0",
-      value: "string",
-      organisationId: "123",
-      type: "string",
-      status: "string",
-      created: "string",
-      updated: "string",
-      assetTeams: [44, 120, 9012, 9012, 9012, 9012, 9012],
-    },
-    {
-      assetId: "1",
-      value: "string",
-      organisationId: "123",
-      type: "string",
-      status: "string",
-      created: "string",
-      updated: "string",
-      assetTeams: [440, 12, 6012, 440, 12, 6012],
-    },
-    {
-      assetId: "2",
-      value: "string",
-      organisationId: "123",
-      type: "string",
-      status: "string",
-      created: "string",
-      updated: "string",
-      assetTeams: [1, 3, 6],
-    },
-    {
-      assetId: "5",
-      value: "string",
-      organisationId: "123",
-      type: "string",
-      status: "string",
-      created: "string",
-      updated: "string",
-      assetTeams: [4, 13, 6],
-    },
-    {
-      assetId: "5",
-      value: "string",
-      organisationId: "123",
-      type: "string",
-      status: "string",
-      created: "string",
-      updated: "string",
-      assetTeams: [44, 1, 1, 1, 1, 2, 612],
-    },
-  ],
-};
+console.log("hello world");
 
-const dedupResult = (array: number[]): number[] => Array.from(new Set(array));
+const num = 1;
+const array = [1];
+array.push(2);
 
-jsonArray.results.forEach((item) => {
-  const arr = item.assetTeams;
-  const depupedArray = dedupResult(arr);
-
-  item.assetTeams = depupedArray;
-});
-
-console.log(...jsonArray.results);
+console.log(num);
